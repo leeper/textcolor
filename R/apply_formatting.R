250872d1ff1b4d4ff94389dde6c1e6bf005f609b
Thomas J. Leeper <thosjleeper@gmail.com>
2017-04-22 16:21:58 +0100

    export apply_formatting()

--- R/apply_formatting.R
+++ R/apply_formatting.R
@@ -1,3 +1,10 @@
+#' @title Repeatedly gsub over a document
+#' @description Introduce formatting by repeatedly gsub-ing over a source text
+#' @param source A character vector
+#' @param pattern A vector of patterns
+#' @param replacement A vector of replacement values
+#' @return A modified version of \code{source}.
+#' @export
 apply_formatting <- 
 function(source, pattern, replacement) {
     if (length(pattern) != length(replacement)) {
@@ -5,7 +12,7 @@ function(source, pattern, replacement) {
     }
     out <- source
     for (i in seq_along(pattern)) {
-        out <- stri_replace_all_fixed(out, pattern[i], replacement[i])
+        out <- stringi::stri_replace_all_fixed(out, pattern[i], replacement[i])
     }
     return(out)
 }
