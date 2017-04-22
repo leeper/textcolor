2dcd6bf6e2a46bf2f0068c91aed636953cfbb49a
Thomas J. Leeper <thosjleeper@gmail.com>
2017-04-22 16:54:29 +0100

    stuff works

--- R/apply_formatting.R
+++ R/apply_formatting.R
@@ -10,8 +10,20 @@ function(source, pattern, open, tokens, close) {
     out <- source
     for (i in seq_along(pattern)) {
         matches <- stringi::stri_extract_all_regex(out, paste0("\\b", pattern[i], "\\b"))[[1L]]
+        # TODO: REPLACE THE BELOW WITH stringi FOR ALL 
         matches <- gsub("\\(", "\\\\(", matches)
         matches <- gsub("\\)", "\\\\)", matches)
+        matches <- gsub("\\[", "\\\\[", matches)
+        matches <- gsub("\\]", "\\\\]", matches)
+        matches <- gsub("\\{", "\\\\{", matches)
+        matches <- gsub("\\}", "\\\\}", matches)
+        matches <- gsub("\\!", "\\\\!", matches)
+        matches <- gsub("\\.", "\\\\.", matches)
+        matches <- gsub("\\$", "\\\\$", matches)
+        matches <- gsub("\\^", "\\\\^", matches)
+        matches <- gsub("\\*", "\\\\*", matches)
+        matches <- gsub("\\+", "\\\\+", matches)
+        matches <- gsub("\\|", "\\\\|", matches)
         if (length(matches) > 0L) {
             for (j in seq_along(matches)) {
                 out <- stringi::stri_replace_all_regex(out, paste0("\\b", matches[j], "\\b"), paste0(open[i], matches[j], close[i]))
